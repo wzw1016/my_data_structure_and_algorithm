d980c4995ef8fb9930212d217a4aa2a0de76b45d
WangZhengWei1016 <wzw487923438@gmail.com>
2020-08-11 00:28:26 +0800

    add DoublyLinkedList.prototype.insert

--- 04.LinkedList/js/DoublyLinkedList.js
+++ 04.LinkedList/js/DoublyLinkedList.js
@@ -24,6 +24,62 @@
       }
       this.length += 1
     }
+
+    DoublyLinkedList.prototype.backwardToString = function() {
+      let current = this.head
+      let resultSting = ''
+      while(current) {
+        const condition = Object.prototype.toString.call(current.data) === '[object Object]' || Object.prototype.toString.call(current.data) === '[object Array]'
+        if (condition) {
+          resultSting += JSON.stringify(current.data) + ' '
+          current = current.next
+        } else {
+          resultSting += current.data + ' '
+          current = current.next
+        }
+      }
+      return resultSting.slice(0, -1)
+    }
+
+    DoublyLinkedList.prototype.forwardToString = function() {
+      let current = this.tail
+      let resultSting = ''
+      while(current) {
+        const condition = Object.prototype.toString.call(current.data) === '[object Object]' || Object.prototype.toString.call(current.data) === '[object Array]'
+        if (condition) {
+          resultSting += JSON.stringify(current.data) + ' '
+          current = current.prev
+        } else {
+          resultSting += current.data + ' '
+          current = current.prev
+        }
+      }
+      return resultSting.slice(0, -1)
+    }
+
+    DoublyLinkedList.prototype.toString = function() {
+      return this.backwardToString()
+    }
+
+    DoublyLinkedList.prototype.insert = function(position, data) {
+      if (position > this.length) return false
+
+      const newNode = new Node(data)
+      let current = this.head
+      let i = 0
+      while(i < position) {
+        current = current.next
+        i += 1
+      }
+      newNode.prev = current.prev
+      current.prev.next = newNode
+
+      newNode.next = current
+      current.prev = newNode
+
+      this.length += 1
+      return true
+    }
   }
 
   window.DoublyLinkedList = DoublyLinkedList
