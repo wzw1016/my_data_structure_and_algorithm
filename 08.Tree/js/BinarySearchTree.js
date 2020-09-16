ae1b4667133179407eca148564fe234fcb70509c
WangZhengWei1016 <wzw487923438@gmail.com>
2020-09-17 00:38:33 +0800

    complete BinarySearchTree

--- 08.Tree/js/BinarySearchTree.js
+++ 08.Tree/js/BinarySearchTree.js
@@ -34,6 +34,7 @@
         }
       }
     }
+    
     // 先序遍历
     BinarySearchTree.prototype.prevOrderTraversal = function(keyHandler) {
       this.prevOrderTraversalNode(this.root, keyHandler)
@@ -86,11 +87,22 @@
       }
     }
 
-
-    /* // 供 prevOrderTraversal / inOrderTraversal / postOrderTraversal 方法使用的进行递归遍历每一个节点的方法
+    /* // 先序遍历
+    BinarySearchTree.prototype.prevOrderTraversal = function(keyHandler) {
+      this.prevOrderTraversalNode(this.root, keyHandler, 'prev')
+    }
+    // 中序遍历
+    BinarySearchTree.prototype.inOrderTraversal = function(keyHandler) {
+      this.inOrderTraversalNode(this.root, keyHandler, 'in')
+    }
+    // 后序遍历
+    BinarySearchTree.prototype.postOrderTraversal = function(keyHandler) {
+      this.postOrderTraversalNode(this.root, keyHandler, 'post')
+    }
+    // 供 prevOrderTraversal / inOrderTraversal / postOrderTraversal 方法使用的进行递归遍历每一个节点的方法
     BinarySearchTree.prototype.traversalNode = function(node, keyHandler, mode) {
       if (node !== null) {
-        // 先序遍历（先依次遍历左子节点）
+        // 先序遍历
         if (mode === 'prev') {
           // 处理节点的key
           keyHandler(node.key)
@@ -100,6 +112,7 @@
           // 递归处理当前节点的右子节点
           this.traversalNode(node.right, keyHandler, mode)
         }
+        // 中序遍历
         else if (mode === 'in') {
           // 递归处理当前节点的右子节点
           this.traversalNode(node.right, keyHandler, mode)
@@ -110,8 +123,8 @@
           // 递归处理当前节点的左子节点
           this.traversalNode(node.left, keyHandler, mode)
         }
-        // 后序遍历（先依次遍历右子节点）
-        else if (mode === 'next') {
+        // 后序遍历
+        else if (mode === 'post') {
           // 递归处理当前节点的右子节点
           this.traversalNode(node.right, keyHandler, mode)
           // 递归处理当前节点的左子节点
@@ -155,7 +168,118 @@
       }
       return false
     }
+
+    BinarySearchTree.prototype.remove = function(key) {
+      let parentNode = null
+      let current = this.root
+      let isParentLeftOrRight = ''
+
+      // 1. 查找移除节点所在位置
+      while (current !== null && current.key !== key) {
+        parentNode = current
+        if (key < current.key) {
+          isParentLeftOrRight = 'left'
+          current = current.left
+        } else {
+          isParentLeftOrRight = 'right'
+          current = current.right
+        }
+      }
+      if (current === null) {
+        return false
+      }
+
+      // 删除节点
+      // 2.1. 被删除节点为叶子节点
+      if (current.left === null && current.right === null) {
+        if (current === this.root) {
+          this.root = null
+        } else {
+          parentNode[isParentLeftOrRight] = null
+        }
+      }
+      // 2.2. 被删除节点为有一个子节点的节点
+      else if (!current.right) {
+        if (current === this.root) {
+          this.root = current.left
+        } else {
+          parentNode[isParentLeftOrRight] = current.left
+        }
+      } else if (!current.left) {
+        if (current.key === this.root) {
+          this.root = current.right
+        } else {
+          parentNode[isParentLeftOrRight] = current.right
+        }
+      }
+      
+      // 2.3. 被删除节点为有两个子节点的节点
+      else if (current.left && current.right) {
+        const successer = this.getSuccessor(current)
+        if (current === this.root) {
+          this.root = successer
+        } else {
+          parentNode[isParentLeftOrRight] = successer
+        }
+        successer.left = current.left
+      }
+    }
+
+
+    BinarySearchTree.prototype.getSuccessor = function(beDeleteNode) {
+      let current = beDeleteNode.right
+      let successer
+      let successerParentNode
+      while (current !== null) {
+        successerParentNode = successer
+        successer = current
+        current = current.left
+      }
+
+      if (successer !== beDeleteNode.right) {
+        successerParentNode.left = successer.right
+        successer.right = beDeleteNode.right
+      }
+
+      return successer
+    }
+
+
   }
 
   window.BinarySearchTree = BinarySearchTree
 })(window)
+
+
+/* 
+
+寻找15的后继节点
+初始化
+current = 15.right = 20
+successer = 15
+successerParentNode = 15
+第一次
+successerParentNode = successer = 15
+successer = current = 20
+current = 20.left = 18
+第二次
+successerParentNode = successer = 20
+successer = current = 18
+current = 18.left = null
+
+20.left = 118.right = 19
+
+*/
+
+
+/* 
+初始化
+current = 15.right = 20
+successer = 15
+successerParentNode = 15
+第一次
+successerParentNode = successer = 15
+successer = current = 20
+current = 20.left = null
+
+*/
